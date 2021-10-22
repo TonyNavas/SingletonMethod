7c6d2574e0433b624d22e6ed91dd1a4c25388aa5
TonyNavas <tonynavas777@gmail.com>
2021-10-22 15:01:09 -0600

    Agregando los atributos de la clase comcercial

--- src/Comercial.java
+++ src/Comercial.java
@@ -1,2 +1,9 @@
 public class Comercial {
+    protected String nombre;
+    protected String direccion;
+    protected String email;
+
+    private static Comercial _instance = null;
+
+    private Comercial(){}
 }

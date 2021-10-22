102b7b90e85130fdeec780051d0d57ca60e5c797
TonyNavas <tonynavas777@gmail.com>
2021-10-22 15:13:50 -0600

    Estableciendo parametros para imprimir comercial

--- src/TestComercial.java
+++ src/TestComercial.java
@@ -1,2 +1,15 @@
 public class TestComercial {
+    public static void main(String[] args)
+    {
+        Comercial comercial = Comercial.Instance();
+        comercial.setNombre("Comercial de Juegos");
+        comercial.setDireccion("Santa Mónica California");
+        comercial.setEmail("support.activition.com");
+        visualiza();
+    }
+    public static void visualiza()
+    {
+        Comercial cm = Comercial.Instance();
+        cm.visualiza();
+    }
 }
